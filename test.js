dc06bf19ed50f7af6ec9b52619f1771cfb5b448b
anonyosmitra <anonyos.mitra@gmail.com>
2023-09-04 01:50:26 +0200

    A

--- test.js
+++ test.js
@@ -1,25 +1,8 @@
-const SetList = require('./SetList.js')
-const fs = require("fs");
-a=new SetList()
-const filePath = 'Maps/neighbour.data';
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading file:', err);
-        return;
-    }
-    data=data.split("\n");
-    var info={}
-    data.forEach(x=>{
-        x=x.slice(1,x.length-1);
-        x=x.split(", ");
-        x=x.map(i=>parseInt(i));
-        console.log(x)
-        info[x[0]]={"West":x[1],"East":x[2],"North West":x[3],"North East":x[4],"South West":x[5],"South East":x[6]}
-    })
-    fs.writeFile(filePath, JSON.stringify(info,null,2), 'utf8', (err) => {
-        if (err) {
-            console.error('Error writing file:', err);
-            return;
-        }});
-});
+const Connection = require('./MongoConnection')
+var con=new Connection();
+var db=con.con.db("Untitled")
+db.collection("Games").deleteMany({})
+db.collection("Gamedata").deleteMany({})
+db.collection("Chats").deleteMany({})
+db.collection("Messages").deleteMany({})
+con.close()
