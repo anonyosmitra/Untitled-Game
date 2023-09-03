06a1a44a23b80f9f847763f5ca274b616f34cc67
anonyosmitra <anonyos.mitra@gmail.com>
2023-09-04 01:58:58 +0200

    A

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-const Connection = require('./MongoConnection')
-var con=new Connection();
-var cols=["Games","Gamedata","Chats","Messages"]
-var db=con.con.db("Untitled")
-cols.forEach(c=>{
-    col=db.collection(c);
-    col.deleteMany({});
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient('mongodb://dbserver.anonyo.net:27017', { useUnifiedTopology: true });
+client.connect(function (){
+    var cols=["Games","Gamedata","Chats","Messages"]
+    var db=client.db("Untitled")
+    cols.forEach(c=>{
+        col=db.collection(c);
+        col.deleteMany({});
+    });
+    client.close()
 });
-con.close()
