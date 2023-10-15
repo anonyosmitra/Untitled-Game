328cc6bbc183203f655075121b71c8c7c7c2c6f7
anonyosmitra <anonyos.mitra@gmail.com>
2023-10-15 11:09:18 +0200

    A

--- static/MapCtrl.js
+++ static/MapCtrl.js
@@ -173,5 +173,5 @@ function showProvincePanel(prov){
 }
 function clc(tile){
     console.log(tile.id);
-    showProvincePanel(tiles[tile.id].prov)
+    showProvincePanel(tiles[parseInt(tile.id.split("-")[1])].prov)
 }
